1b6c221d73e9439bec67f7b12da307a938f35d37
Moses Holmström <thykka@gmail.com>
2021-02-24 00:44:39 +0200

    features!

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
 
+const _ = require('lodash');
+
+const { random, floor, round } = Math;
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -8,11 +12,68 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  console.log(message)
-  if(message.content === 'ping') {
-    message.reply('pong');
-  }
+  if(message.content[0]!=='!') return;
+  processCommand(
+    extractCommand(message.content),
+    message
+  );
 });
+
+function extractCommand(text) {
+  const parts = text.substring(1).split(/\s/g);
+  return {
+    type: parts[0],
+    args: parts.slice(1)
+  };
+}
+
+function processCommand({type, args}, message) {
+  switch(type) {
+    case 'noppa':
+      throwDice(message, args);
+      break;
+    case 'kumpi':
+      chooseOne(message, args);
+    default:
+  }
+}
+
+function throwDice(message, args) {
+  const sides = parseInt(args[0]) || 6;
+  if(sides<=1) {
+    message.reply('Aika kummallinen noppa.')
+    return;
+  }
+  if(sides>200) {
+    message.reply('Heitit palloa.');
+    return;
+  }
+  const result = floor(random() * sides + 1);
+  message.reply(`heitit ${
+    sides===6?'':sides+'-sivuista '
+  }noppaa, sait: ${result}.`);
+}
+
+const choosePrefixes = [
+  'varmaan', 'ehkä', 'oisko', 'no tietysti', 'en tiiä, ehkä', 'en oo varma, mut kenties'
+];
+const chooseSuffixes = [
+ '','','','','','','.','!','?','??','?!','?!'
+];
+
+function chooseOne(message, args) {
+  const choices = args.join(' ').split(/,\s*|\s+vai\s+|\s+tai\s+/g);
+  console.log(choices);
+  if(choices.length<2) {
+    message.reply('täh?');
+    return;
+  }
+  const prefix = _.sample(choosePrefixes);
+  const choice = _.sample(choices);
+  const suffix = _.sample(chooseSuffixes);
+
+  message.reply(`${ prefix } ${ choice }${ suffix }`);
+}
 
 client.login(process.env.TOKEN);
 
