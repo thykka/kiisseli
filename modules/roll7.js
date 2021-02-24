9c54005b897414f0ae3258a6d871eee96a3947d3
Moses Holmström <thykka@gmail.com>
2021-02-24 21:11:31 +0200

    fix modules

--- modules/roll7.js
+++ modules/roll7.js
@@ -1,13 +1,20 @@
 
-function formatDice(result) {
-  return '⚀⚁⚂⚃⚄⚅'[result-1];
-}
+const Storage = require('node-persist');
 
 async function loadRoll7State() {
   const savedState = await Storage.getItem('Roll7_Best');
   return savedState || Infinity;
 }
-let S_Roll7 = await loadRoll7State();
+
+let S_Roll7;
+
+(async function() {
+  S_Roll7 = await loadRoll7State();
+})();
+
+function formatDice(result) {
+  return '⚀⚁⚂⚃⚄⚅'[result-1];
+}
 
 async function processRoll7(message, args) {
   const best = S_Roll7;
@@ -15,7 +22,7 @@ async function processRoll7(message, args) {
     message.channel.send(`Paras heitto: ${best}`)
     return;
   }
-  const roll = () => floor(Math.random()*6)+1;
+  const roll = () => Math.floor(Math.random()*6)+1;
   const dice = Array.from({ length: 7 }, roll);
   const total = dice.reduce((a,d) => a+d, 0);
   const formattedDice = dice.map(d => formatDice(d)).join(' ');
