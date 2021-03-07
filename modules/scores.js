baff7d79e17d9f50049d1bdeefe3c9f8c32633e8
Moses Holmström <thykka@gmail.com>
2021-03-07 18:55:30 +0200

    monospace formatting

--- modules/scores.js
+++ modules/scores.js
@@ -46,12 +46,12 @@ class Scores {
     const scores = await this.storage.getItem(this.getKey());
     const maxNameLength = Object.keys(scores)
       .reduce((acc, name) => Math.max(acc, name.length), 0);
-    return `${ this.name }\n${
+    return `\`\`\`${ this.name }\n${
       Object.entries(scores)
       .sort((a, b) => b[1] - a[1])
       .map(([player, points]) => `${ player.padEnd(maxNameLength, ' ') }: ${ points }`)
       .join('\n')
-    }`;
+    }\`\`\``;
   }
 }
 
