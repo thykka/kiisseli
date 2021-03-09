234c38901e282f3589088bf74182567acc28004e
Moses Holmström <thykka@gmail.com>
2021-03-09 21:41:17 +0200

    reacts

--- modules/reacts.js
+++ modules/reacts.js
@@ -1,7 +1,9 @@
 export default class Reacts {
   constructor(options) {
     const defaults = {
-
+      reactions: [
+        ['Hello, World', '🌐']
+      ]
     };
     Object.assign(this, defaults, options);
   }
@@ -10,7 +12,15 @@ export default class Reacts {
     events.on('message', this.handleMessage.bind(this));
   }
 
-  handleMessage({message}) {
-    console.log(message);
+  handleMessage(message) {
+    this.reactions.forEach(([trigger, emoji]) => {
+      const regex = typeof trigger === 'string'
+        ? new RegExp(`\\b${trigger}\\b`)
+        : trigger;
+      console.log(regex)
+      if(message.content.match(regex)) {
+        message.react(emoji)
+      }
+    })
   }
 }
